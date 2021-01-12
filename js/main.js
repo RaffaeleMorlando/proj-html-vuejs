f633c43cf25d20590b5cba36a4e3187ec74cac62
Raffaele <morlandora@gmail.com>
2021-01-12 15:56:43 +0100

    add item in navbar through v-for

--- js/main.js
+++ js/main.js
@@ -1,7 +1,12 @@
-const { vue } = require("laravel-mix");
+// const { vue } = require("laravel-mix");
 
 const app = new Vue(
   {
-    el: '#app'
+    el: '#app',
+    data: {
+      linkNavbar: ['Home','Courses','About Us','News','Pages','Contact','Purchase'],
+    }
   }
 );
+
+console.log('hello');
