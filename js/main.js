3cd5c2d57e048d136ac4b772151803a5ac39b4e4
Raffaele <morlandora@gmail.com>
2021-01-12 17:51:34 +0100

    section 2 faculties box

--- js/main.js
+++ js/main.js
@@ -5,6 +5,28 @@ const app = new Vue(
     el: '#app',
     data: {
       linkNavbar: ['Home','Courses','About Us','News','Pages','Contact','Purchase'],
+      faculties: [
+        {
+          'img':'img/Gavel-v2.png',
+          'title': 'Law Faculty'
+        },
+        {
+          'img': 'img/Coins-tabs-v2.png',
+          'title': 'Economy'
+        },
+        {
+          'img': 'img/Medicine-tabs-v2.png',
+          'title': 'Medicine'
+        },
+        {
+          'img': 'img/Computer-tabs-v2.png',
+          'title': 'Computer Science'
+        },
+        {
+          'img': 'img/Palette-tabs-v2.png',
+          'title': 'Graphic Design'
+        }
+      ]
     }
   }
 );
